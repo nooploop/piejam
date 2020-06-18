9410bd9b5d959ac5c8764f597d324d85addb4728
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2020-06-18 19:10:54 +0200

    Add a sinusoidal constant power pan algorithm

---
+++ piejam_libs/audio/include/piejam/audio/pan.h
@@ -0,0 +1,66 @@
+// PieJam - An audio mixer for Raspberry Pi.
+//
+// Copyright (C) 2020  Dimitrij Kotrev
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <boost/math/constants/constants.hpp>
+
+namespace piejam::audio
+{
+
+struct stereo_pan
+{
+    float left_gain{};
+    float right_gain{};
+};
+
+// left = 1/sqrt(2) (cos(x×π/4) - sin(x×π/4))
+//     ~= 1/sqrt(2) -
+//           (π x)/(4 sqrt(2)) -
+//           (π^2 x^2)/(32 sqrt(2)) +
+//           (π^3 x^3)/(384 sqrt(2)) +
+//           (π^4 x^4)/(6144 sqrt(2))
+// right = 1/sqrt(2) (cos(x×π/4) + sin(x×π/4))
+//      ~= 1/sqrt(2) +
+//           (π x)/(4 sqrt(2)) -
+//           (π^2 x^2)/(32 sqrt(2)) -
+//           (π^3 x^3)/(384 sqrt(2)) +
+//           (π^4 x^4)/(6144 sqrt(2))
+inline constexpr auto
+sinusoidal_constant_power_pan(float pan_pos) -> stereo_pan
+{
+    using namespace boost::math::float_constants;
+    constexpr float pi_div_4root2 = pi / (4.f * root_two);
+    constexpr float pi_sqr_div_32root2 = pi_sqr / (32.f * root_two);
+    constexpr float pi_cubed_div_384root2 = pi_cubed / (384.f * root_two);
+    constexpr float pi_quad = pi_cubed * pi;
+    constexpr float pi_quad_div_6144root2 = pi_quad / (6144.f * root_two);
+    float const x_sqr = pan_pos * pan_pos;
+    float const x_cubed = x_sqr * pan_pos;
+    float const x_quad = x_cubed * pan_pos;
+    float const ax1 = pi_div_4root2 * pan_pos;
+    float const ax2 = pi_sqr_div_32root2 * x_sqr;
+    float const ax3 = pi_cubed_div_384root2 * x_cubed;
+    float const ax4 = pi_quad_div_6144root2 * x_quad;
+    float const axs = one_div_root_two - ax2 + ax4;
+    float const axt = ax1 - ax3;
+    float const left = axs - axt;
+    float const right = axs + axt;
+    return {left, right};
+}
+
+} // namespace piejam::audio
---
+++ piejam_libs/audio/test/pan_test.cpp
@@ -0,0 +1,58 @@
+// PieJam - An audio mixer for Raspberry Pi.
+//
+// Copyright (C) 2020  Dimitrij Kotrev
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <piejam/audio/pan.h>
+
+#include <gtest/gtest.h>
+
+namespace piejam::audio::test
+{
+
+TEST(sinusoidal_constant_power_pan, minus_one)
+{
+    auto sut = sinusoidal_constant_power_pan(-1.f);
+
+    EXPECT_NEAR(1.f, sut.left_gain, 0.002f);
+    EXPECT_LE(sut.left_gain, 1.f);
+
+    EXPECT_NEAR(0.f, sut.right_gain, 0.002f);
+    EXPECT_GE(sut.right_gain, 0.f);
+}
+
+TEST(sinusoidal_constant_power_pan, zero)
+{
+    auto sut = sinusoidal_constant_power_pan(0.f);
+
+    using namespace boost::math::float_constants;
+
+    EXPECT_FLOAT_EQ(sut.left_gain, sut.right_gain);
+    EXPECT_FLOAT_EQ(one_div_root_two, sut.left_gain);
+    EXPECT_FLOAT_EQ(one_div_root_two, sut.right_gain);
+}
+
+TEST(sinusoidal_constant_power_pan, plus_one)
+{
+    auto sut = sinusoidal_constant_power_pan(1.f);
+
+    EXPECT_NEAR(0.f, sut.left_gain, 0.002f);
+    EXPECT_GE(sut.left_gain, 0.f);
+
+    EXPECT_NEAR(1.f, sut.right_gain, 0.002f);
+    EXPECT_LE(sut.right_gain, 1.f);
+}
+
+} // namespace piejam::audio::test
