e14c886d9fe467ad94c72726025062c6a64cdc34
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2020-08-06 00:24:27 +0200

    Fix compile errors in pan_test.cpp

--- piejam_libs/audio/test/pan_test.cpp
+++ piejam_libs/audio/test/pan_test.cpp
@@ -26,11 +26,11 @@ TEST(sinusoidal_constant_power_pan, minus_one)
 {
     auto sut = sinusoidal_constant_power_pan(-1.f);
 
-    EXPECT_NEAR(1.f, sut.left(), 0.002f);
-    EXPECT_LE(sut.left(), 1.f);
+    EXPECT_NEAR(1.f, sut.left, 0.002f);
+    EXPECT_LE(sut.left, 1.f);
 
-    EXPECT_NEAR(0.f, sut.right(), 0.002f);
-    EXPECT_GE(sut.right(), 0.f);
+    EXPECT_NEAR(0.f, sut.right, 0.002f);
+    EXPECT_GE(sut.right, 0.f);
 }
 
 TEST(sinusoidal_constant_power_pan, zero)
@@ -39,20 +39,20 @@ TEST(sinusoidal_constant_power_pan, zero)
 
     using namespace boost::math::float_constants;
 
-    EXPECT_FLOAT_EQ(sut.left(), sut.right());
-    EXPECT_FLOAT_EQ(one_div_root_two, sut.left());
-    EXPECT_FLOAT_EQ(one_div_root_two, sut.right());
+    EXPECT_FLOAT_EQ(sut.left, sut.right);
+    EXPECT_FLOAT_EQ(one_div_root_two, sut.left);
+    EXPECT_FLOAT_EQ(one_div_root_two, sut.right);
 }
 
 TEST(sinusoidal_constant_power_pan, plus_one)
 {
     auto sut = sinusoidal_constant_power_pan(1.f);
 
-    EXPECT_NEAR(0.f, sut.left(), 0.002f);
-    EXPECT_GE(sut.left(), 0.f);
+    EXPECT_NEAR(0.f, sut.left, 0.002f);
+    EXPECT_GE(sut.left, 0.f);
 
-    EXPECT_NEAR(1.f, sut.right(), 0.002f);
-    EXPECT_LE(sut.right(), 1.f);
+    EXPECT_NEAR(1.f, sut.right, 0.002f);
+    EXPECT_LE(sut.right, 1.f);
 }
 
 } // namespace piejam::audio::test
