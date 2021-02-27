cc2be61ab47b918046fb6335be748c49046bdb2e
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2021-02-27 21:26:52 +0100

    partial_compare: use reversed comparison for less/greater comparer
    To make it more intuitive in a curried application, we need to use
    reversed compare operation underneath, for less/greater operations.
    e.g. less(5)(4) should be true, which would use greater operation underneath

--- piejam_libs/functional/include/piejam/functional/partial_compare.h
+++ piejam_libs/functional/include/piejam/functional/partial_compare.h
@@ -12,11 +12,10 @@ namespace piejam
 {
 
 inline constexpr auto equal_to = boost::hof::partial(std::equal_to<>{});
-inline constexpr auto less = boost::hof::partial(std::less<>{});
-inline constexpr auto less_equal = boost::hof::partial(std::less_equal<>{});
-inline constexpr auto greater = boost::hof::partial(std::greater<>{});
-inline constexpr auto greater_equal =
-        boost::hof::partial(std::greater_equal<>{});
+inline constexpr auto less = boost::hof::partial(std::greater<>{});
+inline constexpr auto less_equal = boost::hof::partial(std::greater_equal<>{});
+inline constexpr auto greater = boost::hof::partial(std::less<>{});
+inline constexpr auto greater_equal = boost::hof::partial(std::less_equal<>{});
 inline constexpr auto not_equal_to = boost::hof::partial(std::not_equal_to<>{});
 
 } // namespace piejam
---
+++ piejam_libs/functional/tests/partial_compare_test.cpp
@@ -0,0 +1,52 @@
+// PieJam - An audio mixer for Raspberry Pi.
+// SPDX-FileCopyrightText: 2020  Dimitrij Kotrev
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <piejam/functional/partial_compare.h>
+
+#include <gtest/gtest.h>
+
+namespace piejam::test
+{
+
+TEST(partial_compare, equal_to)
+{
+    EXPECT_TRUE(equal_to(5)(5));
+    EXPECT_FALSE(equal_to(5)(6));
+}
+
+TEST(partial_compare, not_equal_to)
+{
+    EXPECT_TRUE(not_equal_to(5)(6));
+    EXPECT_FALSE(not_equal_to(5)(5));
+}
+
+TEST(partial_compare, less)
+{
+    EXPECT_FALSE(less(5)(6));
+    EXPECT_FALSE(less(5)(5));
+    EXPECT_TRUE(less(5)(4));
+}
+
+TEST(partial_compare, less_equal)
+{
+    EXPECT_FALSE(less(5)(6));
+    EXPECT_TRUE(less_equal(5)(5));
+    EXPECT_TRUE(less_equal(5)(4));
+}
+
+TEST(partial_compare, greater)
+{
+    EXPECT_FALSE(greater(5)(4));
+    EXPECT_FALSE(greater(5)(5));
+    EXPECT_TRUE(greater(5)(6));
+}
+
+TEST(partial_compare, greater_equal)
+{
+    EXPECT_FALSE(greater_equal(5)(4));
+    EXPECT_TRUE(greater_equal(5)(5));
+    EXPECT_TRUE(greater_equal(5)(6));
+}
+
+} // namespace piejam::test
