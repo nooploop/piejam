ad601f6a039b62691085af06e42ca01e4dbb5041
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2021-02-28 10:33:01 +0100

    Revert "Revert "Remove algorithm::unique_erase, boost::unique_erase can be used instead""
    This reverts commit 8ab242a0f18ce1c8f99f34ab6f78f08f642a2099.

--- piejam_libs/runtime/src/piejam/runtime/midi_control_middleware.cpp
+++ piejam_libs/runtime/src/piejam/runtime/midi_control_middleware.cpp
@@ -6,7 +6,6 @@
 
 #include <piejam/algorithm/contains.h>
 #include <piejam/algorithm/for_each_visit.h>
-#include <piejam/algorithm/unique_erase.h>
 #include <piejam/midi/device_update.h>
 #include <piejam/runtime/actions/activate_midi_device.h>
 #include <piejam/runtime/actions/apply_app_config.h>
@@ -197,7 +196,7 @@ midi_control_middleware::process_midi_control_action(
     }
 
     std::ranges::sort(m_enabled_devices);
-    algorithm::unique_erase(m_enabled_devices);
+    boost::unique_erase(m_enabled_devices);
 
     next(action);
 }
