1c11f60a960081ed620b05d874fbac3400ed13fd
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2021-03-14 10:31:13 +0100

    Add missing copyright notices

--- piejam_libs/thread/src/piejam/thread/semaphore.cpp
+++ piejam_libs/thread/src/piejam/thread/semaphore.cpp
@@ -1,3 +1,7 @@
+// PieJam - An audio mixer for Raspberry Pi.
+// SPDX-FileCopyrightText: 2021  Dimitrij Kotrev
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <piejam/thread/semaphore.h>
 
 #include <semaphore.h>
