1a1fdf1d5e8106b29ef8cf0d317784ba02f092e9
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2021-04-16 14:01:48 +0200

    math.h: add dB_to_linear, add min_linear argument to linear_to_dB

--- piejam_libs/base/include/piejam/math.h
+++ piejam_libs/base/include/piejam/math.h
@@ -20,11 +20,21 @@ pow3(T const x) noexcept -> T
 
 template <std::floating_point T>
 constexpr auto
-to_dB(T const l) -> T
+linear_to_dB(T const linear, T const min_linear = T{}) -> T
 {
     static_assert(std::numeric_limits<T>::is_iec559, "IEEE 754 required");
-    constexpr auto const fact = T{20} / std::log(T{10});
-    return l == T{} ? -std::numeric_limits<T>::infinity() : std::log(l) * fact;
+    constexpr auto const twenty_div_log_10 = T{20} / std::log(T{10});
+    return linear <= min_linear ? -std::numeric_limits<T>::infinity()
+                                : std::log(linear) * twenty_div_log_10;
+}
+
+template <std::floating_point T>
+constexpr auto
+dB_to_linear(T const dB) -> T
+{
+    return dB == -std::numeric_limits<T>::infinity()
+                   ? T{}
+                   : std::pow(T{10}, dB / T{20});
 }
 
 template <class T>
