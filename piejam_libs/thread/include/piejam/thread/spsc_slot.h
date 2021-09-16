de1f92b8103a98492eff1d2a83d00e8e4bb7b31d
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2021-09-16 18:58:23 +0200

    spsc_slot: tighten noexcept requirements

--- piejam_libs/thread/include/piejam/thread/spsc_slot.h
+++ piejam_libs/thread/include/piejam/thread/spsc_slot.h
@@ -8,6 +8,7 @@
 
 #include <array>
 #include <atomic>
+#include <concepts>
 #include <functional>
 #include <type_traits>
 
@@ -20,19 +21,20 @@ class spsc_slot
     static_assert(std::atomic_size_t::is_always_lock_free);
 
 public:
-    void push(T const& v) noexcept(std::is_nothrow_copy_assignable_v<T>)
+    void push(T const& v) noexcept
+            requires(std::is_nothrow_copy_assignable_v<T>)
     {
         m_store[m_write].value = v;
         commit();
     }
 
-    void push(T&& v) noexcept(std::is_nothrow_move_assignable_v<T>)
+    void push(T&& v) noexcept requires(std::is_nothrow_move_assignable_v<T>)
     {
         m_store[m_write].value = std::move(v);
         commit();
     }
 
-    template <class F>
+    template <std::invocable<T const&> F>
     void consume(F&& f) noexcept(
             noexcept(std::invoke(std::forward<F>(f), std::declval<T>())))
     {
@@ -44,10 +46,10 @@ public:
         }
     }
 
-    bool pull(T& r) noexcept(std::is_nothrow_copy_assignable_v<T>)
+    bool pull(T& r) noexcept requires(std::is_nothrow_copy_assignable_v<T>)
     {
         bool pulled{};
-        consume([&r, &pulled](T const& value) {
+        consume([&r, &pulled](T const& value) noexcept {
             r = value;
             pulled = true;
         });
@@ -62,9 +64,20 @@ private:
                 m_swap_pos.exchange(mark(m_write), std::memory_order_acq_rel));
     }
 
-    static auto mark(std::size_t x) -> std::size_t { return x | 0b100; }
-    static bool marked(std::size_t x) { return (x & 0b100); }
-    static auto pos(std::size_t x) -> std::size_t { return x & 0b11; }
+    static constexpr auto mark(std::size_t const x) noexcept -> std::size_t
+    {
+        return x | 0b100;
+    }
+
+    static constexpr bool marked(std::size_t const x) noexcept
+    {
+        return (x & 0b100);
+    }
+
+    static constexpr auto pos(std::size_t const x) noexcept -> std::size_t
+    {
+        return x & 0b11;
+    }
 
     alignas(cache_line_size) std::atomic_size_t m_swap_pos{1};
     alignas(cache_line_size) std::size_t m_read{0};
