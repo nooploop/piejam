a9846ccf209210f9daacbd56e3d24f9ead8b11a8
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2022-05-18 21:15:20 +0200

    algorithm: restrict template arguments to all_of_adjacent with concepts

--- piejam_libs/algorithm/include/piejam/algorithm/all_of_adjacent.h
+++ piejam_libs/algorithm/include/piejam/algorithm/all_of_adjacent.h
@@ -4,28 +4,34 @@
 
 #pragma once
 
+#include <piejam/algorithm/concepts.h>
+
 #include <algorithm>
-#include <concepts>
+#include <functional>
 #include <iterator>
 #include <ranges>
 
 namespace piejam::algorithm
 {
 
-template <std::input_iterator It, class BinaryPredicate>
-constexpr bool
-all_of_adjacent(It&& first, It&& last, BinaryPredicate&& p)
+template <
+        std::forward_iterator It,
+        std::indirect_binary_predicate<It, It> BinaryPredicate>
+constexpr auto
+all_of_adjacent(It&& first, It&& last, BinaryPredicate&& p) -> bool
 {
     return std::forward<It>(last) ==
-           std::adjacent_find(
+           std::ranges::adjacent_find(
                    std::forward<It>(first),
                    std::forward<It>(last),
                    std::not_fn(std::forward<BinaryPredicate>(p)));
 }
 
-template <std::ranges::input_range Range, class BinaryPredicate>
-constexpr bool
-all_of_adjacent(Range&& rng, BinaryPredicate&& p)
+template <
+        std::ranges::forward_range Range,
+        range_binary_predicate<Range> BinaryPredicate>
+constexpr auto
+all_of_adjacent(Range&& rng, BinaryPredicate&& p) -> bool
 {
     using std::begin;
     using std::end;
