1f8a18673db13f375e3a3b8f61ce5e88cb4015e6
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2023-04-28 17:10:49 +0200

    audio: add make_graph_endpoint utility

--- piejam_libs/audio/include/piejam/audio/engine/graph_endpoint.h
+++ piejam_libs/audio/include/piejam/audio/engine/graph_endpoint.h
@@ -18,17 +18,22 @@ struct graph_endpoint
 
     auto operator<(graph_endpoint const& other) const noexcept -> bool
     {
-        auto const proc_l = std::addressof(proc.get());
-        auto const proc_r = std::addressof(other.proc.get());
+        auto* const proc_l = std::addressof(proc.get());
+        auto* const proc_r = std::addressof(other.proc.get());
         return proc_l < proc_r || (proc_l == proc_r && port < other.port);
     }
 
     auto operator==(graph_endpoint const& other) const noexcept -> bool
     {
-        auto const proc_l = std::addressof(proc.get());
-        auto const proc_r = std::addressof(other.proc.get());
+        auto* const proc_l = std::addressof(proc.get());
+        auto* const proc_r = std::addressof(other.proc.get());
         return proc_l == proc_r && port == other.port;
     }
+};
+
+template <std::size_t Port>
+inline constexpr auto make_graph_endpoint = [](processor& proc) {
+    return graph_endpoint{.proc = proc, .port = Port};
 };
 
 } // namespace piejam::audio::engine
