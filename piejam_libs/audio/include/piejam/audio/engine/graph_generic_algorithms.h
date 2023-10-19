595610073285d8123ed2df9fa605d44f812d02c7
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2023-10-19 19:29:28 +0200

    audio: add connect_event_to
    connect_event_to connects multiple source nodes with all their
    event outputs sequentially to a target node

--- piejam_libs/audio/include/piejam/audio/engine/graph_generic_algorithms.h
+++ piejam_libs/audio/include/piejam/audio/engine/graph_generic_algorithms.h
@@ -8,11 +8,11 @@
 #include <piejam/audio/engine/graph.h>
 #include <piejam/audio/engine/graph_node.h>
 
-#include <piejam/algorithm/transform_to_vector.h>
 #include <piejam/range/indices.h>
 
 #include <boost/assert.hpp>
 
+#include <algorithm>
 #include <memory>
 #include <utility>
 #include <vector>
@@ -40,9 +40,9 @@ template <graph_node Src, graph_node Dst>
 void
 connect(graph& g, Src&& src, Dst&& dst)
 {
-    BOOST_ASSERT(src.num_outputs() == dst.num_inputs());
-
-    for (std::size_t p = 0, e = src.num_outputs(); p < e; ++p)
+    for (std::size_t p = 0, e = std::min(src.num_outputs(), dst.num_inputs());
+         p < e;
+         ++p)
     {
         g.audio.insert(
                 src_endpoint(std::forward<Src>(src), p),
@@ -67,22 +67,56 @@ connect_event(
      ...);
 }
 
-template <graph_node Node>
-auto
-in_event_endpoints(Node&& node)
+template <graph_node Dst, graph_node... Src>
+void
+connect_event_to(graph& g, Dst&& dst, Src&&... src)
 {
-    return algorithm::transform_to_vector(
-            range::indices(node.event_inputs()),
-            [&](std::size_t i) { return dst_event_endpoint(node, i); });
+    auto connect_single =
+            [&]<class SingleSrc>(SingleSrc&& s, std::size_t& dst_port) {
+                for (std::size_t src_port : range::indices(s.event_outputs()))
+                {
+                    g.event.insert(
+                            src_event_endpoint(s, src_port),
+                            dst_event_endpoint(dst, dst_port++));
+                }
+            };
+
+    std::size_t dst_port{};
+    (connect_single(src, dst_port), ...);
 }
 
-template <graph_node Node>
+template <graph_node... Node>
 auto
-out_event_endpoints(Node&& node)
+in_event_endpoints(Node&&... node)
 {
-    return algorithm::transform_to_vector(
-            range::indices(node.event_outputs()),
-            [&](std::size_t i) { return src_event_endpoint(node, i); });
+    std::vector<graph_endpoint> result;
+
+    (std::ranges::transform(
+             range::indices(node.event_inputs()),
+             std::back_inserter(result),
+             std::bind_front(
+                     &in_event_endpoint<Node>,
+                     std::ref(std::forward<Node>(node)))),
+     ...);
+
+    return result;
+}
+
+template <graph_node... Node>
+auto
+out_event_endpoints(Node&&... node)
+{
+    std::vector<graph_endpoint> result;
+
+    (std::ranges::transform(
+             range::indices(node.event_outputs()),
+             std::back_inserter(result),
+             std::bind_front(
+                     &out_event_endpoint<Node>,
+                     std::ref(std::forward<Node>(node)))),
+     ...);
+
+    return result;
 }
 
 } // namespace piejam::audio::engine
