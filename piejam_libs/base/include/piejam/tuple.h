1754a37278bf46d0fdf98393cc4d7d6b3d91f548
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2024-09-15 12:16:44 +0200

    base: add tuple iteration utilities (for_each_while/for_each_until)

--- piejam_libs/base/include/piejam/tuple.h
+++ piejam_libs/base/include/piejam/tuple.h
@@ -12,4 +12,70 @@ namespace piejam::tuple
 template <class Tuple>
 using decay_elements_t = boost::mp11::mp_transform<std::decay_t, Tuple>;
 
+namespace detail
+{
+
+template <class Tp, std::size_t... J, class F>
+constexpr auto
+for_each_while_impl(Tp&& tp, std::integer_sequence<std::size_t, J...>, F&& f)
+        -> bool
+{
+    return (f(get<J>(std::forward<Tp>(tp))) && ...);
+}
+
+template <class Tp, class F>
+constexpr auto
+for_each_while_impl(Tp&& /*tp*/, std::integer_sequence<std::size_t>, F&& /*f*/)
+        -> bool
+{
+    return true;
+}
+
+} // namespace detail
+
+template <class Tp, class F>
+auto
+for_each_while(Tp&& tp, F&& f) -> bool
+{
+    using seq = std::make_index_sequence<
+            std::tuple_size<typename std::remove_reference<Tp>::type>::value>;
+    return detail::for_each_while_impl(
+            std::forward<Tp>(tp),
+            seq{},
+            std::forward<F>(f));
+}
+
+namespace detail
+{
+
+template <class Tp, std::size_t... J, class F>
+constexpr auto
+for_each_until_impl(Tp&& tp, std::integer_sequence<std::size_t, J...>, F&& f)
+        -> bool
+{
+    return (f(get<J>(std::forward<Tp>(tp))) || ...);
+}
+
+template <class Tp, class F>
+constexpr auto
+for_each_until_impl(Tp&& /*tp*/, std::integer_sequence<std::size_t>, F&& /*f*/)
+        -> bool
+{
+    return false;
+}
+
+} // namespace detail
+
+template <class Tp, class F>
+auto
+for_each_until(Tp&& tp, F&& f) -> bool
+{
+    using seq = std::make_index_sequence<
+            std::tuple_size<typename std::remove_reference<Tp>::type>::value>;
+    return detail::for_each_until_impl(
+            std::forward<Tp>(tp),
+            seq{},
+            std::forward<F>(f));
+}
+
 } // namespace piejam::tuple
