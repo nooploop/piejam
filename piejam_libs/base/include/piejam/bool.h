1645aef7cdf5879522ec93d71c5fc53b6e71d61c
Dimitrij Kotrev <dimitrij.kotrev@googlemail.com>
2024-09-20 11:48:56 +0200

    base: add toggle_bool utility

---
+++ piejam_libs/base/include/piejam/bool.h
@@ -0,0 +1,16 @@
+// PieJam - An audio mixer for Raspberry Pi.
+// SPDX-FileCopyrightText: 2024  Dimitrij Kotrev
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+namespace piejam
+{
+
+constexpr inline auto
+toggle_bool(bool& x) -> bool
+{
+    return x = !x;
+}
+
+} // namespace piejam
